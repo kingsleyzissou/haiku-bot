ff6c1c8f2b024883d2ce6c7a5e7efb484dee66ad
Gianluca <gianlucazuccarelli@gmail.com>
2021-03-05 21:24:39 +0000

    chore: housekeeping

--- src/twitter.py
+++ src/twitter.py
@@ -11,10 +11,14 @@ consumer_secret = os.getenv("CONSUMER_SECRET")
 access_token = os.getenv("ACCESS_TOKEN")
 access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
 
+
 def create_api():
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+    api = tweepy.API(
+        auth, wait_on_rate_limit=True,
+        wait_on_rate_limit_notify=True
+    )
     try:
         api.verify_credentials()
     except Exception as e:
