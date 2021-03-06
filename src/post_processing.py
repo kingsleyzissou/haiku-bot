71d0fde514ab95c9c859cd06031c5e73274273e3
Gianluca <gianlucazuccarelli@gmail.com>
2021-03-06 01:16:59 +0000

    fix: pre & post processing

--- src/post_processing.py
+++ src/post_processing.py
@@ -4,8 +4,8 @@ def post_process(text):
     This method is a little bit hacky, but
     it works
     """
-    text = ' '.join(text) ## remove whitespace
-    text = text.split() ## split again
-    text = ' '.join(text) ## and repeat
-    text = text.rstrip() ## remove trailing whitespace
-    return text.capitalize() ## return capitalized phrase
+    text = ' '.join(text)  # remove whitespace
+    text = text.split()  # split again
+    text = ' '.join(text)  # and repeat
+    text = text.rstrip()  # remove trailing whitespace
+    return text  # return capitalized phrase
